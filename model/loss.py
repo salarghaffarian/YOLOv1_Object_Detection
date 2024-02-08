5880570a0b6f3e011085ad715f900056b6b5967b
Salar Ghaffarian <salarghaffarian1363@gmail.com>
2024-02-08 13:05:43 -0600

    Add comments

--- model/loss.py
+++ model/loss.py
@@ -6,11 +6,11 @@ class YoloLoss(nn.Module):
     def __init__(self, S=7, B=2, C=20):
         super(YoloLoss, self).__init__()
         self.mse = nn.MSELoss(reduction='sum')
-        self.S = S
-        self.B = B
-        self.C = C
-        self.lambda_noobj = 0.5
-        self.lambda_coord = 5
+        self.S = S   # split the image into SxS grid cells
+        self.B = B   # number of bounding boxes in each grid cell
+        self.C = C   # number of classes
+        self.lambda_noobj = 0.5   # weight for the no object loss
+        self.lambda_coord = 5     # weight for the bounding box coordinates loss
 
     def forward(self, predictions, target):
         predictions = predictions.reshape(-1, self.S, self.S, self.C + self.B*5)
